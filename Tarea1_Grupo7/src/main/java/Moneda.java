267c6e79c8dc30b6208feb6fe6f236274c58f930
Bastian Vásquez <bvasquez2025@udec.cl>
2026-04-29 17:42:29 -0400

    Override método compareTo en Monedas, compara valores

--- Tarea1_Grupo7/src/main/java/Moneda.java
+++ Tarea1_Grupo7/src/main/java/Moneda.java
@@ -4,5 +4,10 @@ public abstract class Moneda implements Comparable<Moneda>{
 	return this; //se usa el hashcode para identificar a la Moneda
 	}
 	public abstract int getValor();
+	
+    @Override
+	public int compareTo(Moneda entrada){//Aqui se han de comparar los valores entre dos monedas
 
+		return Integer.compare(this.getValor(), entrada.getValor());
+	}
 }
