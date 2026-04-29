e83bed24e0ff5a5c44f8de5139d1245073036460
Bastian Vásquez <bvasquez2025@udec.cl>
2026-04-29 17:38:17 -0400

    Implementar interfaz Comparable en Moneda

--- Tarea1_Grupo7/src/main/java/Moneda.java
+++ Tarea1_Grupo7/src/main/java/Moneda.java
@@ -1,7 +1,8 @@
-public abstract class Moneda{
+public abstract class Moneda implements Comparable<Moneda>{
 	public Moneda(){} //Moneda solo necista usar this como identificador
 	public Moneda getSerie(){
 	return this; //se usa el hashcode para identificar a la Moneda
 	}
 	public abstract int getValor();
+
 }
