e6efb30ef7de8836188a08f6d7a96e732c7b6d64
Bastian Vásquez <bvasquez2025@udec.cl>
2026-04-29 17:58:15 -0400

    Crear método constructor para Comprador, añadir comentarios

--- Tarea1_Grupo7/src/main/java/Comprador.java
+++ Tarea1_Grupo7/src/main/java/Comprador.java
@@ -1,6 +1,12 @@
 public class Comprador{ //El comprador debe de recibir una moneda y el nombre del producto que se desea comprar
 			//Luego, ha de intentar comprar el producto
 			//Finalmente, devolver el vuelto correspondiente con Monedas100
-
-
+	public Comprador(Moneda pago, int productoID, Expendedor refExp){
+		//Recibe una moneda, entero identificador del producto y una referencia a un Expendedor
+		//Compra en el expendedor usando la moneda pago el producto productoID, ej: productoRecivido = refExp.Comprar(pago, productoID);
+		//luego, si se consiguio un producto hay que consumirlo y registrar su sabor, ejemplo: sabos = productoRecibido.Consumir();
+		//Luego recuperar la moneda del vuelto una a una
+		//NO almacenar las monedas ni los productos como propiedad
+		//Unicas propiedades: un string (sabor) y un entero 
+	}
 }
