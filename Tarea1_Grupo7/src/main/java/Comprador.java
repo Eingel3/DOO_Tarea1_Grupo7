952a082d5a295ee8e05e651c312b023e155f36fa
Bastian Vásquez <bvasquez2025@udec.cl>
2026-04-28 22:15:56 -0400

    Crear clase Comprador.java

---
+++ Tarea1_Grupo7/src/main/java/Comprador.java
@@ -0,0 +1,6 @@
+public class Comprador{ //El comprador debe de recibir una moneda y el nombre del producto que se desea comprar
+			//Luego, ha de intentar comprar el producto
+			//Finalmente, devolver el vuelto correspondiente con Monedas100
+
+
+}
