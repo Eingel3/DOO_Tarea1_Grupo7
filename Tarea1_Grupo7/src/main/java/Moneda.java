3ab4cde1c91da897e1099f2a08525d9f8c7590d7
Bastian Vásquez <bvasquez2025@udec.cl>
2026-04-28 21:44:39 -0400

    Crear constructor, métodos getSerie y getValor en clase Moneda

--- Tarea1_Grupo7/src/main/java/Moneda.java
+++ Tarea1_Grupo7/src/main/java/Moneda.java
@@ -1,3 +1,7 @@
 public abstract class Moneda{
-
+	public Moneda(){} //Moneda solo necista usar this como identificador
+	public Moneda getSerie(){
+	return this; //se usa el hashcode para identificar a la Moneda
+	}
+	public abstract int getValor();
 }
