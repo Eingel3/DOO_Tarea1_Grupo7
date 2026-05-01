76a2e85f0ee0b7767b60cd18a6bc3c264c53f258
Bastian Vásquez <bvasquez2025@udec.cl>
2026-05-01 01:19:19 -0400

    En Deposito, crear metodo addObjeto(T)

--- Tarea1_Grupo7/src/main/java/Deposito.java
+++ Tarea1_Grupo7/src/main/java/Deposito.java
@@ -4,4 +4,7 @@ public class Deposito<T>{
 	public Deposito(){
 		almacen = new ArrayList<>();
 	}
+	public void addObjeto(T objeto){ //Obtenemos un objeto generico
+		almacen.add(objeto); //Se agrega el objeto generico, ya sea Moneda, Bebida o Dulce		
+	}
 }
