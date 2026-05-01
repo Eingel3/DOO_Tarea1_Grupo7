4e8a8997c73c2e7caf1838f33454509c0cf10deb
Bastian Vásquez <bvasquez2025@udec.cl>
2026-05-01 01:22:59 -0400

    En Deposito crear método getObjeto()

--- Tarea1_Grupo7/src/main/java/Deposito.java
+++ Tarea1_Grupo7/src/main/java/Deposito.java
@@ -7,4 +7,11 @@ public class Deposito<T>{
 	public void addObjeto(T objeto){ //Obtenemos un objeto generico
 		almacen.add(objeto); //Se agrega el objeto generico, ya sea Moneda, Bebida o Dulce		
 	}
+
+	public T getObjeto() { //Retorna una Moneda o Bebida o Dulce
+        if (almacen.size()==0) { //Si el almacen no tiene objetos, retornamos null
+            return null;
+        }
+        return almacen.remove(0); //Si tiene un objeto, removemos el objeto del almacen y lo retornamos
+    }
 }
