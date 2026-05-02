3a66f00d6474ceea84b9f1e1bcf1d0ad0007883f
Bastian Vásquez <bvasquez2025@udec.cl>
2026-05-02 11:36:30 -0400

    Crear en Deposito un deposito demonedas

--- Tarea1_Grupo7/src/main/java/Expendedor.java
+++ Tarea1_Grupo7/src/main/java/Expendedor.java
@@ -5,12 +5,16 @@ public class Expendedor {
 	private Deposito<Super8> super8;
 	private Deposito<Sprite> sprite;
 
+    private Deposito<Moneda> monedaDeposito;
+
 	public Expendedor(int cantidad){
 		coca = new Deposito<CocaCola>();
         sprite = new Deposito<Sprite>();
         fanta = new Deposito<Fanta>();
         snicker= new Deposito<Snicker>();
         super8 = new Deposito<Super8>();
+
+        monedaDeposito = new Deposito<Moneda>();
         
         for (int i = 0; i<cantidad; i++){
             Bebida temp = new CocaCola(1100+i);
@@ -25,11 +29,11 @@ public class Expendedor {
             coca.addObjeto(temp);
         }
         for (int i = 0; i<cantidad; i++){
-            Dulce temp = new Super8(200+i);
+            Dulce temp = new Super8(2100+i);
             sprite.addObjeto(temp);
         }   
         for (int i = 0; i<cantidad; i++){
-            Dulce temp = new Snicker(200+i);
+            Dulce temp = new Snicker(2200+i);
             sprite.addObjeto(temp);
         }         
 
