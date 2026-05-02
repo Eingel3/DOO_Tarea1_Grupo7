be47543c746177359fe8c07d3e9494bde1d10e15
Bastian Vásquez <bvasquez2025@udec.cl>
2026-05-02 18:17:48 -0400

    Crear prueba simple en Main para compar y consumir una sprite

--- Tarea1_Grupo7/src/main/java/Main.java
+++ Tarea1_Grupo7/src/main/java/Main.java
@@ -2,7 +2,14 @@ import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args){
+		Expendedor exp = new Expendedor(2);
+		System.out.println("Expendedor con 2 de cada producto creado");
+        System.out.println("Comprar Sprite con Moneda500, precio exacto: ");
+        Comprador c = new Comprador(new Moneda500(), InformacionProducto.SPRITE.getTipo(), exp);
+        System.out.println("Consumido: " + c.compradorConsumido());
+        System.out.println("Vuelto: " + c.compradorVuelto());
 
     }
+
 	
 }
