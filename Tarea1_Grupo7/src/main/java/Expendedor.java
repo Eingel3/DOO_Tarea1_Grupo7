a837ea5fe32c9adbbb1d741818d9a3a70f01cd1d
Bastian Vásquez <bvasquez2025@udec.cl>
2026-05-02 00:55:27 -0400

    Crer constructor para Expendedor

--- Tarea1_Grupo7/src/main/java/Expendedor.java
+++ Tarea1_Grupo7/src/main/java/Expendedor.java
@@ -1,9 +1,37 @@
 public class Expendedor {
 	private Deposito<Fanta> fanta;
 	private Deposito<CocaCola> coca;
-	private Deposito<Snicker> fanta;
+	private Deposito<Snicker> snicker;
 	private Deposito<Super8> super8;
 	private Deposito<Sprite> sprite;
 
+	public Expendedor(int cantidad){
+		coca = new Deposito<CocaCola>();
+        sprite = new Deposito<Sprite>();
+        fanta = new Deposito<Fanta>();
+        snicker= new Deposito<Snicker>();
+        super8 = new Deposito<Super8>();
+        
+        for (int i = 0; i<cantidad; i++){
+            Bebida temp = new CocaCola(1100+i);
+            coca.addObjeto(temp);
+        }
+        for (int i = 0; i<cantidad; i++){
+            Bebida temp = new Sprite(1200+i);
+            sprite.addObjeto(temp);
+        }
+        for (int i = 0; i<cantidad; i++){
+            Bebida temp = new Fanta(1300+i);
+            coca.addObjeto(temp);
+        }
+        for (int i = 0; i<cantidad; i++){
+            Dulce temp = new Super8(200+i);
+            sprite.addObjeto(temp);
+        }   
+        for (int i = 0; i<cantidad; i++){
+            Dulce temp = new Snicker(200+i);
+            sprite.addObjeto(temp);
+        }         
 
+	}
 }
