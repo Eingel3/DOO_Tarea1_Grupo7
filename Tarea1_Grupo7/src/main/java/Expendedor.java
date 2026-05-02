7ee78cfbe35671784e0776b418792c19d5016d69
Bastian Vásquez <bvasquez2025@udec.cl>
2026-05-02 00:40:55 -0400

    En expendedor: Crear depositos privados de productos

--- Tarea1_Grupo7/src/main/java/Expendedor.java
+++ Tarea1_Grupo7/src/main/java/Expendedor.java
@@ -1,3 +1,9 @@
 public class Expendedor {
+	private Deposito<Fanta> fanta;
+	private Deposito<CocaCola> coca;
+	private Deposito<Snicker> fanta;
+	private Deposito<Super8> super8;
+	private Deposito<Sprite> sprite;
+
 
 }
