44662a80827be6c66c6427caa5ed6f400bfe029e
Bastian Vásquez <bvasquez2025@udec.cl>
2026-05-02 14:48:56 -0400

    En comprador, manejar las posibles excepciones

--- Tarea1_Grupo7/src/main/java/Comprador.java
+++ Tarea1_Grupo7/src/main/java/Comprador.java
@@ -1,10 +1,14 @@
+import Expendedor.NoHayProductoException;
+import Expendedor.PagoIncorrectoException;
+import Expendedor.PagoInsuficienteException;
+
 public class Comprador{ //El comprador debe de recibir una moneda y el nombre del producto que se desea comprar
 			//Luego, ha de intentar comprar el producto
 			//Finalmente, devolver el vuelto correspondiente con Monedas100
 
 	private String consumido; //que se ha consumido
 	private int vuelto;
-	
+
 	public Comprador(Moneda pago, int productoID, Expendedor refExp){
 		//Recibe una moneda, entero identificador del producto y una referencia a un Expendedor
 		//Compra en el expendedor usando la moneda pago el producto productoID, ej: productoRecivido = refExp.Comprar(pago, productoID);
@@ -13,6 +17,23 @@ public class Comprador{ //El comprador debe de recibir una moneda y el nombre de
 		//NO almacenar las monedas ni los productos como propiedad
 		//Unicas propiedades: un string (sabor) y un entero 
         vuelto = 0;
+        try {
+        Producto comprado = refExp.comprarProducto(productoID, pago); 
+        }
+        catch (PagoIncorrectoException e){// No se devuelve nada, vuelt es 0 y producto null
+        	System.out.println("Pago incorrecto: " + e.getMessage()); //e.getMessage() obtiene como String el mesnaje del exception
+        	consumido = "No se ha consumido nada.";
+        }
+        catch (PagoInsuficienteException e ){ //solo se devuelve la misma moneda con la que se intento pagar
+        	System.out.println("Pago insuficiente: " + e.getMessage());
+        	consumido = "No se ha consumido nada.";
+        	vuelto = pago.getValor();
+        }
+        catch (NoHayProductoException e){ //solo se devuelve la misma moneda con la que se intento pagar
+        	System.out.println("No hay existencias del producto: " + e.getMessage());
+        	consumido = "No se ha consumido nada.";
+        	vuelto = pago.getValor();
+        }
 
 	}
 }
