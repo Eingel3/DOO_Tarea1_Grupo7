814c6a07ff2fafdc04d0166b74593331d997a944
Bastian Vásquez <bvasquez2025@udec.cl>
2026-05-01 22:39:51 -0400

    Crear constantes y constructor de enum PrecioProducto

--- Tarea1_Grupo7/src/main/java/PrecioProducto.java
+++ Tarea1_Grupo7/src/main/java/PrecioProducto.java
@@ -1,3 +1,12 @@
 public enum PrecioProducto {
+	SPRITE(500), //SPRITE llama a su constructor mediante 500
+	COCACOLA(500),
+	SNICKER(100),
+	SUPER8(100),
+	FANTA(500);
+	private final int precio;
 
+	private PrecioProducto(int precio) {
+		this.precio = precio;
+		}
 }
