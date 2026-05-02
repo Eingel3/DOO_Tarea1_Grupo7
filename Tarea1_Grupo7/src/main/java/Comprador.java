1cd54b83507ab5bdff64133bc6a46f1887e0ab55
Bastian Vásquez <bvasquez2025@udec.cl>
2026-05-02 14:26:06 -0400

    Crear variables consumido y vuelto en Comprador

--- Tarea1_Grupo7/src/main/java/Comprador.java
+++ Tarea1_Grupo7/src/main/java/Comprador.java
@@ -1,6 +1,10 @@
 public class Comprador{ //El comprador debe de recibir una moneda y el nombre del producto que se desea comprar
 			//Luego, ha de intentar comprar el producto
 			//Finalmente, devolver el vuelto correspondiente con Monedas100
+
+	private String consumido; //que se ha consumido
+	private int vuelto;
+	
 	public Comprador(Moneda pago, int productoID, Expendedor refExp){
 		//Recibe una moneda, entero identificador del producto y una referencia a un Expendedor
 		//Compra en el expendedor usando la moneda pago el producto productoID, ej: productoRecivido = refExp.Comprar(pago, productoID);
@@ -8,5 +12,7 @@ public class Comprador{ //El comprador debe de recibir una moneda y el nombre de
 		//Luego recuperar la moneda del vuelto una a una
 		//NO almacenar las monedas ni los productos como propiedad
 		//Unicas propiedades: un string (sabor) y un entero 
+        vuelto = 0;
+
 	}
 }
