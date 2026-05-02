19388c80e38acdd80f611d38b6b74cabadeeb854
Bastian Vásquez <bvasquez2025@udec.cl>
2026-05-02 17:09:14 -0400

    Borrar imports innecesarios de Comprador

--- Tarea1_Grupo7/src/main/java/Comprador.java
+++ Tarea1_Grupo7/src/main/java/Comprador.java
@@ -1,7 +1,3 @@
-import Expendedor.NoHayProductoException;
-import Expendedor.PagoIncorrectoException;
-import Expendedor.PagoInsuficienteException;
-
 public class Comprador{ //El comprador debe de recibir una moneda y el nombre del producto que se desea comprar
 			//Luego, ha de intentar comprar el producto
 			//Finalmente, devolver el vuelto correspondiente con Monedas100
