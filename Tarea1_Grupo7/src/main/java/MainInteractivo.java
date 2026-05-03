2d88b71c85dcf668a091ef51988b8a6e4bb6f2d1
Bastian Vásquez <bvasquez2025@udec.cl>
2026-05-02 22:03:16 -0400

    Crear mensaje bienvenida en  MainInteractivo

--- Tarea1_Grupo7/src/main/java/MainInteractivo.java
+++ Tarea1_Grupo7/src/main/java/MainInteractivo.java
@@ -2,7 +2,13 @@ import java.util.Scanner;
 
 public class MainInteractivo {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         Expendedor exp = new Expendedor(3); // 3 unidades de cada producto
-
+        System.out.println("Bienvenid@");
+        System.out.println("Productos disponibles:");
+        System.out.println(" - Bebidas: 11-Sprite, 12-CocaCola, 13-Fanta (precio 500)");
+        System.out.println(" - Dulces:  21-Snicker, 22-Super8 (precio 500)");
+        System.out.println("Monedas válidas: 100, 500, 1000");
+        System.out.println("Escriba 'salir' para terminar.\n");
     }
 }
