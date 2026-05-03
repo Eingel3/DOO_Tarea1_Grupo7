a9ffd0fd82d05fabfadebbc1f79a4b6f6afc6f49
Bastian Vásquez <bvasquez2025@udec.cl>
2026-05-02 21:58:04 -0400

    Crear expendedor exp en  MainInteractivo

--- Tarea1_Grupo7/src/main/java/MainInteractivo.java
+++ Tarea1_Grupo7/src/main/java/MainInteractivo.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class MainInteractivo {
     public static void main(String[] args){
+        Expendedor exp = new Expendedor(3); // 3 unidades de cada producto
 
     }
 }
